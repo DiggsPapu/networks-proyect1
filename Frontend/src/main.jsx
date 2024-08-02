8b3de2f995c4363be23fd62bf0b15439bff17a5e
DiggsPapu <diggs.papu@gmail.com>
2024-08-01 19:38:49 -0600

    feat:Login&SignIn
    Created pages for login and Sign in

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom"
+import Login from './pages/login.jsx'
+import SignIn from './pages/SignIn.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<App/>),
+  },
+  {
+    path: "/login",
+    element: (<Login/>),
+  },
+  {
+    path: "/signIn",
+    element: (<SignIn/>),
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
