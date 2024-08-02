49b1e4ba263faecba43cc8a6317813b20ee79b1c
DiggsPapu <diggs.papu@gmail.com>
2024-08-01 19:19:25 -0600

    feat: Chat 1.0
    Version 1.0 of the chat, just talking and stuff

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,150 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component, Fragment } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import ChatHead from './components/ChatHead'
+import Message from './components/Message'
+import ChatFooter from './components/ChatFooter'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Hola toro
-      </p>
-    </>
-  )
+import GlobalStyle from './GlobalStyle'
+import styled from 'styled-components'
+
+const heightToggled = `
+    height: 0%;
+    min-height: 0%;
+    opacity: 0;
+`
+
+const height = `
+    height: 60%;
+    opacity: 1;
+`
+
+const heightContainerToggled = `
+    height: 100px;
+`
+
+const heightContainer = `
+    height: 500px;
+`
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    width: 100%;
+    height: 100%;
+`;
+
+export const ChatContainer = styled.div`
+    width: 500px;
+    background-color: rgba(0,0,0,0.4) !important;
+    border-radius: 4px 4px 0px 0px;
+    flex-direction: colunm;
+    margin-right: 10px;
+
+    ${props => props.toggle === true ? heightContainerToggled : heightContainer}
+	transition: all 0.5s ease;
+`
+
+export const ChatBody = styled.div`
+    width: 100%;
+    height: 60%;
+    min-height: 60%;
+    max-height: 60%;
+    overflow: auto;
+
+    ${props => props.toggle === true ? heightToggled : height}
+`;
+
+const body = ({toggle, children}) => (
+    <ChatBody
+        toggle={toggle}
+    >
+        {children}
+    </ChatBody>
+)
+
+const container = ({toggle, children}) => (
+    <ChatContainer
+        toggle={toggle}
+    >
+        {children}
+    </ChatContainer>
+)
+
+
+function randomName() {
+  const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
+  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return `${adjective.toUpperCase()} ${noun.toUpperCase()}`;
 }
 
-export default App
+function getQuote(numberMessages) {
+  var nouns = ["car", "horse", "apple", "person", "chimp"];
+  var adjectives = ["red", "fast", "lonely", "hungry", "insane"];
+
+  var messages = [];
+
+  for (var i = 0; i <= numberMessages; i++) {
+      var randomNounIndex = Math.floor(Math.random() * nouns.length);
+      var randomAdjectiveIndex = Math.floor(Math.random() * adjectives.length);
+      var retVal = i % 2 === 0 ?
+                    `The ${nouns[randomNounIndex]} is ${adjectives[randomAdjectiveIndex]} and 
+                    ${nouns[randomNounIndex]} is ${adjectives[randomAdjectiveIndex]} and
+                    ${nouns[randomNounIndex]} is ${adjectives[randomAdjectiveIndex]}`
+                  : 
+                    `${nouns[randomNounIndex]} is ${adjectives[randomAdjectiveIndex]}`;
+      messages.push({userId: i % 2 === 0 ? 1 : 2, message: retVal})
+  }
+
+  return messages;
+}
+
+class App extends Component {
+  state = {
+    name: randomName(),
+    messages: getQuote(),
+    toggleChat: false
+  }
+
+  onSubmitMessage = msg => {
+    this.setState((prevState) => {
+      const newMessage = {userId: 1, message:msg }
+      return {   
+        messages: [...prevState.messages, newMessage]
+      }
+    })
+  }
+
+  handleToggle = () => {
+    this.setState({toggleChat: !this.state.toggleChat})
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <GlobalStyle />
+        <Container>
+          <ChatContainer
+            toggle={this.state.toggleChat}
+          >
+            <ChatHead 
+              name={this.state.name} 
+              toggle={this.state.toggleChat}
+              onClick={this.handleToggle}
+            />
+            <ChatBody toggle={this.state.toggleChat}>
+              <Message messages={this.state.messages} />
+            </ChatBody>
+            <ChatFooter 
+              toggle={this.state.toggleChat}
+              onSubmitMessage={this.onSubmitMessage}/>
+          </ChatContainer>
+        </Container>        
+      </Fragment>
+    );
+  }
+}
+
+export default App;
